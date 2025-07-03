d9aecb4562362b2e5e6886bcb6a9e765e73010af
Lee seohuyn <103621624+SeoHyun1024@users.noreply.github.com>
2025-07-03 17:34:15 +0900

    [Silver IV] Title: ATM, Time: 220 ms, Memory: 21336 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,31 +1,30 @@
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 사람 수 입력
         int peopleNum = sc.nextInt();
-        sc.nextLine();
-
-        String line = sc.nextLine();
-
         Integer[] times = new Integer[peopleNum];
 
+        // 입력: 한 사람씩 바로 받기
         for (int i = 0; i < peopleNum; i++) {
-            times[i] =Integer.parseInt(line.split(" ")[i]);
+            times[i] = sc.nextInt();
         }
-        Arrays.sort(times, Comparator.naturalOrder());
 
+        // 오름차순 정렬
+        Arrays.sort(times);
 
-        int totalSum = 0;
-        int individualSum = 0;
-        for (int i : times){
-            individualSum += i;
-            totalSum += individualSum;
+        int totalSum = 0;   // 필요한 시간의 최솟값
+        int accumulatedSum = 0; // 누적합
+
+        for (int time : times) {
+            accumulatedSum += time;
+            totalSum += accumulatedSum;
         }
+
         System.out.println(totalSum);
     }
 }
