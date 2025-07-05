ff5ff33035c9088652069ca56d7100681940d084
Lee seohuyn <103621624+SeoHyun1024@users.noreply.github.com>
2025-07-06 02:43:39 +0900

    [Silver III] Title: 주유소, Time: 948 ms, Memory: 185576 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -6,40 +6,26 @@ public class Main {
 
         // 정수 입력
         int n = sc.nextInt();   // 도시 갯수
-        Integer[] distances = new Integer[n - 1]; // 도시 간 거리
-        Integer[] prices = new Integer[n]; // 리터 당 가격
+        Long[] distances = new Long[n - 1]; // 도시 간 거리
+        Long[] prices = new Long[n]; // 리터 당 가격
 
         for (int i = 0; i < n - 1; i++) {
-            distances[i] = sc.nextInt();
+            distances[i] = sc.nextLong();
         }
         for (int i = 0; i < n; i++) {
-            prices[i] = sc.nextInt();
+            prices[i] = sc.nextLong();
         }
 
-        int min_price = prices[0];  // 최솟값 초기화
-        int total_distance = 0; // 총 거리
-        int distance_pasted = 0;  // 온 거리
-        int total_price = 0;    // 총 가격
+        long min_price = prices[0];  // 최솟값 초기화
+        long total_price = 0;    // 총 가격
 
         // 가격 최솟값 계산
         for (int i = 0; i < n - 1; i++) {
             if (min_price > prices[i]) {
                 min_price = prices[i];
             }
-            total_distance += distances[i]; // 총 거리 합 계산
+            total_price += min_price * distances[i];
         }
-
-        // 가격의 최소 합 계산
-        for (int i = 0; i < n - 1; i++) {
-            if (prices[i] > min_price) {
-                total_price += distances[i] * prices[i];
-            } else {
-                total_price += (total_distance - distance_pasted) * prices[i];
-                break;
-            }
-            distance_pasted += distances[i];    // 지나온 거리 계산
-        }
-
         System.out.println(total_price);
     }
 }
