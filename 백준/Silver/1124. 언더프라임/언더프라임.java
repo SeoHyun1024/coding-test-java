edae50b6b0d11734ea99cadcc25f9261b93d92a0
Lee seohuyn <103621624+SeoHyun1024@users.noreply.github.com>
2025-07-14 15:49:50 +0900

    [Silver I] Title: 언더프라임, Time: 2400 ms, Memory: 40984 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.java
+++ 백준/Silver/1124. 언더프라임/언더프라임.java
@@ -8,26 +8,27 @@ public class Main {
         // 정수 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        int N = Integer.parseInt(input.split(" ")[0]);
-        int M = Integer.parseInt(input.split(" ")[1]);
+        int N = Integer.parseInt(input.split(" ")[0]);  // 시작 정수 (N) 입력
+        int M = Integer.parseInt(input.split(" ")[1]);  // 끝 정수 (M) 입력
 
-        int count = 0;
+        int count = 0;  // 언더 프라임 갯수
         Map<Integer, ArrayList<Integer>> map = new HashMap<>();
 
         for (int i = N; i <= M; i++) {
             int num = i, j = 1;
             map.put(i, new ArrayList<>());  // map 초기화
-            
+
             while (num > 1) {
                 j++;
                 if (num % j == 0) { // 나눠 떨어지면
                     num /= j;
                     map.get(i).add(j);  // map 에 추가
-                    j = 1;
+                    j = 1;  // j 값 초기화
                 }
             }
         }
 
+        // 언더프라임인 정수 갯수 카운트
         for (int i = N; i <= M; i++) {
             int size = map.get(i).size();
             if (isPrime(size)) count++;
@@ -36,7 +37,7 @@ public class Main {
         // 갯수 출력
         System.out.println(count);
     }
-    
+
     // 소수인지 판별하는 함수
     public static boolean isPrime(int n) {
         int count = 0;
