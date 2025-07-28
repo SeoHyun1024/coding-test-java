a219ff1ab788dfee1c5862b12384c196b1b19975
Lee seohuyn <103621624+SeoHyun1024@users.noreply.github.com>
2025-07-29 03:11:42 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int verificationNum = 0;
+        for (int i = 0; i < 5; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            verificationNum += x * x;
+        }
+        verificationNum %= 10;
+        System.out.println(verificationNum);
+    }
+}
