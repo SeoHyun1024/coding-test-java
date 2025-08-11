16e69fc224e8d29858e691fc551d8546f3db4a9c
Lee seohuyn <103621624+SeoHyun1024@users.noreply.github.com>
2025-08-12 01:18:55 +0900

    [Bronze V] Title: A+B - 2, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static int A, B;
+    
+	public static void main (String[] args) throws java.lang.Exception {
+	    Scanner in = new Scanner(System.in);
+		A = in.nextInt();
+		B = in.nextInt();
+		System.out.print(A + B);
+	}
+}
