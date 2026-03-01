e2d0bb4a6a45792db86afe25cdd2c7a3595761ab
Lee seohyeon <103621624+SeoHyun1024@users.noreply.github.com>
2026-03-01 16:06:41 +0900

    [Bronze V] Title: A+B - 8, Time: 216 ms, Memory: 19076 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+	    Scanner sc = new Scanner(System.in);
+	    int N = sc.nextInt();
+	    
+	    for(int i = 0; i < N; i++){
+	       int a = sc.nextInt();
+	       int b = sc.nextInt();
+	       System.out.println("Case #" + (i+1) + ": " + a + " + " + b + " = " + (a+b));    
+	    }
+	}
+}
