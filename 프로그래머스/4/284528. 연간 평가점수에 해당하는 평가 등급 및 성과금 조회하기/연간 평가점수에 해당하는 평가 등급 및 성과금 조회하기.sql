7c5ecdd8e6cbc87de27846eb928fb9362c29cb58
Lee seohyeon <103621624+SeoHyun1024@users.noreply.github.com>
2026-03-06 00:07:39 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,18 @@
+SELECT E.EMP_NO, E.EMP_NAME, G.GRADE,
+    (E.SAL * G.PERCENTAGE) AS BONUS
+FROM HR_EMPLOYEES E JOIN (
+    SELECT EMP_NO, 
+        CASE WHEN AVG(SCORE) >= 96 THEN "S"
+             WHEN AVG(SCORE) >= 90 THEN "A"
+             WHEN AVG(SCORE) >= 80 THEN "B"
+             ELSE "C"
+        END AS GRADE,
+        CASE WHEN AVG(SCORE) >= 96 THEN 0.2
+             WHEN AVG(SCORE) >= 90 THEN 0.15
+             WHEN AVG(SCORE) >= 80 THEN 0.1
+             ELSE 0
+        END AS PERCENTAGE
+    FROM HR_GRADE
+    GROUP BY EMP_NO) G
+ON E.EMP_NO = G.EMP_NO
+ORDER BY E.EMP_NO
