648050ef936a8d05d01ca71a5b0d103082e875e9
Lee seohyeon <103621624+SeoHyun1024@users.noreply.github.com>
2026-04-03 17:50:46 +0900

    [Gold IV] Title: 최단경로, Time: 696 ms, Memory: 108560 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -3,83 +3,87 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    public static long[] distance;
-    public static int[] prev;
-    public static List<List<Edge>> graph;
-    public static int V, E, K;
-    
-    // 간선 클래스 정의
+    // 간선 정의
     static class Edge{
-        int destination;
-        int weight;
+        int to;
+        int cost;
         
-        Edge(int destination, int weight){
-            this.destination = destination;
-            this.weight = weight;
+        Edge(int to, int cost){
+            this.to = to;
+            this.cost = cost;
         }
     }
     
+    static int V, E;
+    static int start;
+    static List<Edge>[] graph;
+    static long[] dist;
+    static final long INF = Long.MAX_VALUE;
+    
 	public static void main (String[] args) throws java.lang.Exception {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st ;
 	    
-	    // V, E 입력
-	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    V = Integer.parseInt(st.nextToken());   // 노드 갯수
+	    st = new StringTokenizer(br.readLine());
+	    V = Integer.parseInt(st.nextToken());   // 정점 갯수
 	    E = Integer.parseInt(st.nextToken());   // 간선 갯수
-	    K = Integer.parseInt(br.readLine());   // 시작 정점 번호
 	    
-	    graph = new ArrayList<>(V + 1); // 그래프 초기화
+	    start = Integer.parseInt(br.readLine());   // 시작 정점 번호
 	    
-	    for (int i = 0; i <= V; i++) graph.add(new ArrayList<>());
+	    graph = new ArrayList[V + 1];
+	    dist = new long[V + 1];
 	    
-	    // 배열 초기화
-        distance = new long[V + 1];
-        prev = new int[V + 1];
-        Arrays.fill(distance, Long.MAX_VALUE / 4);
-        Arrays.fill(prev, -1);
-        
-        for (int i = 0; i < E; i++){
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st1.nextToken()) - 1;
-            int v = Integer.parseInt(st1.nextToken()) - 1;
-            int w = Integer.parseInt(st1.nextToken());
-            
-            graph.get(u).add(new Edge(v, w));   // 간선 추가
-        }
-        
-        dijkstra(K - 1);
-        
-        for (int i  = 0; i < V; i++){
-            if (distance[i] == Long.MAX_VALUE / 4){
-                System.out.println("INF");
-            }else{
-                System.out.println(distance[i]);
-            }
-        }
+	    // 그래프 초기화
+	    for(int i = 1; i <= V; i++){
+	        graph[i] = new ArrayList<>();
+	    }
+	    
+	    // 그래프 입력
+	    for(int i = 0; i < E; i++){
+	        st = new StringTokenizer(br.readLine());
+	        // u -> v 가중치 w인 간선 추가
+	        int u = Integer.parseInt(st.nextToken());
+	        int v = Integer.parseInt(st.nextToken());
+	        int w = Integer.parseInt(st.nextToken());
+	        
+	        graph[u].add(new Edge(v, w));
+	    }
+	    
+	    dijkstra(start);
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for(int i = 1; i <= V; i++){
+	        if(dist[i] == INF) sb.append("INF\n");
+	        else sb.append(dist[i]).append("\n");
+	    }
+	    System.out.print(sb);
 	}
 	
 	public static void dijkstra(int start){
-	    PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
-	    distance[start] = 0;
-	    pq.offer(new long[]{0L, start});
+	    // dist 배열 초기화
+	    Arrays.fill(dist, INF);
+	    dist[start] = 0;
+	    
+	    PriorityQueue<Edge> pq = new PriorityQueue<>((a,b) -> Long.compare(a.cost, b.cost));
+	    pq.offer(new Edge(start, 0));
 	    
 	    while(!pq.isEmpty()){
-	        long[] cur = pq.poll();
-	        long d = cur[0];
-	        int u = (int) cur[1];
+	        Edge now = pq.poll();
 	        
-	        if (d != distance[u]) continue; // 더 좋은 경우로 갱신된 경우 스킵
+	        int current = now.to;
+	        long cost = now.cost;
 	        
-	        for (Edge e : graph.get(u)){
-	            int v = e.destination;
-	            long nd = d + e.weight; // 가중치 합산하여 새로운 거리 저장
+	        if(cost > dist[current]) continue;
+	        
+	        for(Edge next : graph[current]){
+	            long nextCost = cost + next.cost;
 	            
-	            if (nd < distance[v]){
-	                distance[v] = nd;
-	                prev[v] = u;
-	                pq.offer(new long[]{nd, v});
+	            if(nextCost < dist[next.to]){
+	                dist[next.to] = nextCost;
+	                pq.offer(new Edge(next.to, (int) nextCost));
 	            }
 	        }
 	    }
+	   
 	}
 }
