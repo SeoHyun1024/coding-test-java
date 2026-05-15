f2fa2a40b51e1e863eec401c3b91a9729454cb25
Lee seohyeon <103621624+SeoHyun1024@users.noreply.github.com>
2026-05-15 18:08:08 +0900

    [level 0] Title: 배열 두배 만들기, Time: 1.55 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/README.md
+++ 프로그래머스/0/120809. 배열 두배 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 두배 만들기 - 120809 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120809) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120809#) 
 
 ### 성능 요약
 
-메모리: 65.6 MB, 시간: 0.02 ms
+메모리: 60.4 MB, 시간: 1.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 15일 17:58:12
+2026년 05월 15일 18:08:05
 
 ### 문제 설명
 
--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -2,13 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        int size = numbers.length;
-        int[] answer = new int[size];
-        
-        for(int i = 0; i < size; i++){
-            answer[i] = numbers[i]*2;
-        }
-        
-        return answer;
+        return Arrays.stream(numbers).map(x -> x*2).toArray();
     }
 }
