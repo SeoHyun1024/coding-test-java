48b67efd8baa8b017bbfa0ea9d290b02c3c16614
Lee seohyeon <103621624+SeoHyun1024@users.noreply.github.com>
2026-05-21 14:48:38 +0900

    [level 2] Title: H-Index, Time: 4.17 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,15 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-
-        for (int i = 1; i <= citations.length; i++) {
-            int count = i;
-            for (int j : citations) {
-                if (j >= i) count--;
+                
+        
+        for(int i = 1; i <= citations.length; i++){
+            int cnt = i;
+            for(int j : citations){
+                if(i <= j) cnt--;
             }
-            if (count <= 0) answer = i;
+            if(cnt <= 0) answer = i;
         }
-
+                
         return answer;
     }
 }
